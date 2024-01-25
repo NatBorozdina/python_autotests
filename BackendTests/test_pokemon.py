75fa2e85b1b36bdd6a2d048ba6b38d027b28752a
Natalya Borozdina <natalya-shashko@yandex.ru>
2024-01-25 20:30:38 +0700

    Исправлены автотесты

--- BackendTests/test_pokemon.py
+++ BackendTests/test_pokemon.py
@@ -7,9 +7,17 @@ HEADER = {"Content-Type": "application/json",
           "trainer_token": "fc5fcec17a1ff31db059bc66f344a975"
           }
 
-def test_get_trainers():
+def test_get_trainers_by_level():
     '''
-    KTI-1 Get trainers
+    KTI Get trainers by level
+    '''
+    response = requests.get(url=f'{URL}/trainers', params={'level':1}, headers=HEADER, timeout=10)
+    assert response.status_code == 200, 'Unexpected status code'
+
+def test_get_trainers_by_id():
+    '''
+    KTI-2 Get trainer by id
     '''
     response = requests.get(url=f'{URL}/trainers', params={'trainer_id':3617}, headers=HEADER, timeout=10)
     assert response.status_code == 200, 'Unexpected status code'
+    assert response.json()['trainer_name'] == 'Наталья', 'Unexpected name'
